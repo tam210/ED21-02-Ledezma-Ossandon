4ff07a94a7cd48bbcd824a4efd3b0f36ebcc91b5
tam210 <91286473+tam210@users.noreply.github.com>
2021-10-11 00:40:07 -0300

    Creación main.cpp
    Código que implementa la primera historia de usuario del taller, correspondiente al release 0.1

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+using namespace cv;             // FALTA DOCUMENTAR
+using namespace std;
+
+void main() {
+	string path = "Resources/test_video2.mp4";
+	VideoCapture cap(path);
+	Mat img;
+	while (true) {
+		cap.read(img);
+		CascadeClassifier faceCascade;
+		faceCascade.load("Resources/haarcascade_frontalface_default.xml");
+		vector<Rect> faces;
+		faceCascade.detectMultiScale(img, faces, 1.1, 20);
+		for (int i = 0; i < faces.size(); i++) {
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 0, 255), 3);
+		}
+		imshow("Image", img);
+		waitKey(5);
+	}
+}
