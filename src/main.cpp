696e0c690b45d02e51e6aa5dcc1e694fb6ec8aef
tam210 <91286473+tam210@users.noreply.github.com>
2021-10-23 23:54:13 -0300

    Update main.cpp
    Se actualizan las otras listas

--- src/main.cpp
+++ src/main.cpp
@@ -61,3 +61,200 @@ int main() {
         }
     }
 }
+
+
+#include <opencv2/opencv.hpp>
+using namespace std;
+
+
+class Node
+{
+public:
+	Node();
+	Node(Mat);
+	~Node();
+	Mat data;
+	Node* next;
+
+
+};
+
+
+
+#include <opencv2/opencv.hpp>
+class Node { //podria ser un struct
+public:
+    Mat data;
+    Node* next;
+
+    Node()
+    {
+        data = 0;
+        next = nullptr;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+
+
+#include <opencv2/opencv.hpp>
+#include "Node.cpp"
+using namespace std;
+
+
+class Linkedlist {
+
+    struct Node { //podria ser un struct
+    public:
+        Mat data;
+        Node* next;
+
+        Node()
+        {
+            data = NULL;
+            next = nullptr;
+        }
+
+        Node(int data)
+        {
+            this->data = data;
+            this->next = nullptr;
+        }
+    };
+
+    Node* head;
+
+public:
+    Linkedlist() {
+        head = nullptr;
+    }
+
+
+    void insertNode(Mat);
+
+    void printList();
+
+
+    void deleteNode(Mat);
+};
+
+
+
+#include "LinkedList.hpp"
+#include <iostream>
+
+using namespace std;
+//Funcion para eliminar el nodo en la posicion dada
+void Linkedlist::deleteNode(int nodeOffset)
+{
+    Node* temp1 = head, * temp2 = nullptr;
+    int ListLen = 0;
+
+    if (head == nullptr) {
+        cout << "Lista vacia" << endl;
+        return;
+    }
+
+    // Encuentra el tamaño de la linked list
+    while (temp1 != nullptr) {
+        temp1 = temp1->next;
+        ListLen++;
+    }
+
+    //Ve si la posicion debe ser eliminada en menos del tamaño
+    // //de la linked list
+    if (ListLen < nodeOffset) {
+        cout << "Indice fuera de rango"
+            << endl;
+        return;
+    }
+
+    // Declare temp1
+    temp1 = head;
+
+    // Borrando head
+    if (nodeOffset == 1) {
+
+        // Update head
+        head = head->next;
+        delete temp1;
+        return;
+    }
+
+    // Viaja por la lista buscando el nodo a eliminar
+    while (nodeOffset-- > 1) {
+
+        // Update temp2
+        temp2 = temp1;
+
+        // Update temp1
+        temp1 = temp1->next;
+    }
+
+    // Cambia puntero
+    temp2->next = temp1->next;
+
+    // Delete the node
+    delete temp1;
+}
+
+// Insertar nodo
+void Linkedlist::insertNode(int data)
+{
+    // Crea nodo
+    Node* newNode = new Node(data);
+
+    // Asigna head
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    // Recorre hasta el fin de la lista
+    Node* temp = head;
+    while (temp->next != nullptr) {
+
+        // Actualiza temp
+        temp = temp->next;
+    }
+
+    // Insert at the last.
+    temp->next = newNode;
+}
+
+//Funcion que imprime los nodos de la linked list
+void Linkedlist::printList()
+{
+    Node* temp = head;
+
+    // Check for empty list.
+    if (head == nullptr) {
+        cout << "List empty" << endl;
+        return;
+    }
+
+ 
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
